157a9acaa07d43e4943293c15ac4ad5b5caaf945
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 10:19:09 +0100

    Added the logo

---
+++ pages/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+    return (
+        <div>
+            <img src="/logo.png" alt="Evolve Training Logo" className="h-20" />
+        </div>
+    )
+}
