f961472c7b6b3914bdac4f4d04e42ec5d24529e6
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 12:13:11 +0100

    Changed to a dynamic date

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function Title() {
+
+    const date = new Date();
+
+    // Returns the month number with "0" in front if it is less than 10
+    const getMonth = (month: number) => {
+        if(month < 10) {
+            return "0" + month.toString();
+        } else {
+            return month.toString();
+        }
+    }
+
     return (
         <div>
-            <h1 className="text-4xl text-darkblue font-bold">Forklift Handeling - 14/08/2022</h1>
+            <h1 className="text-4xl text-darkblue font-bold">Forklift Handeling - {date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear()}</h1>
         </div>
     );
 }
