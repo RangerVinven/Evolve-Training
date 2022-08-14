522bc5573c7c7778e7b543e175b61a72750a8794
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 11:48:59 +0100

    Made the logo center on mobile

--- pages/components/Logo.tsx
+++ pages/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Logo() {
     return (
-        <div>
+        <div className="w-full flex items-center justify-center md:block lg:block">
             <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-1" />
         </div>
     )
