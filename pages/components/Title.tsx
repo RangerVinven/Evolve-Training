1786329f8f3aab53fe56545ac214adeed4427c72
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 10:35:01 +0100

    Changed the size, weight and colour of the title

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
     return (
         <div>
-            <h1 className="text-lg">Forklift Handeling - 14/08/2022</h1>
+            <h1 className="text-4xl text-darkblue font-bold">Forklift Handeling - 14/08/2022</h1>
         </div>
     );
 }
