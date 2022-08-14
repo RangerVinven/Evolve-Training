7950f56740229e08ccca19b3fd02e4c82c046875
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 10:30:29 +0100

    Added the Form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 
+import Logo from './components/Logo'
+import Form from './components/Form'
+
 const Home: NextPage = () => {
 	return (
 		<div>
-			
+			<Logo />
+			<div className="flex items-center justify-center">
+				<Form />
+			</div>
 		</div>
 	)
 }
