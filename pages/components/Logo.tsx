7e5bec9debaa9bbc0db5c3259d6eeeb771d83487
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 11:28:13 +0100

    Added margins

--- pages/components/Logo.tsx
+++ pages/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
     return (
         <div>
-            <img src="/logo.png" alt="Evolve Training Logo" className="h-20" />
+            <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-1" />
         </div>
     )
 }
