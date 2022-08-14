25dea488b369848803cf902ec192565a273fbb51
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-14 10:26:53 +0100

    Added the title

---
+++ pages/components/Title.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div>
+            <h1 className="text-lg">Forklift Handeling - 14/08/2022</h1>
+        </div>
+    );
+}
