66e7e5575af6edad6e0c929df46ea24b280bfb57
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-15 21:06:04 +0100

    Added a message

--- pages/components/CourseNotFound.tsx
+++ pages/components/CourseNotFound.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Title from './Title';
 
 export default function CourseNotFound() {
+
+	const paragraphStyling = "text-green text-lg font-bold mt-1";
+
 	return (
-		<div>
-			<Title title="Course Not Found" showDate={false} />	
+		<div className="flex flex-col justify-center items-center">
+			<Title title="Course Not Found" showDate={false} />
+			<p className={paragraphStyling}>
+				Please make sure you entered the URL exactly as shown below the QR code.
+			</p>
 		</div>
 	)
 }
