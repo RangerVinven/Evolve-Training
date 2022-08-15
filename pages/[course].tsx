2355018733478efcb1873cc59b0ce2a481edf75c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-15 19:23:56 +0100

    Added the dynamic route

---
+++ pages/[course].tsx
@@ -0,0 +1,50 @@
+import type { NextPage } from 'next'
+import { PrismaClient } from '@prisma/client';
+import { useRouter } from 'next/router';
+
+import Logo from './components/Logo'
+import Form from './components/Form'
+import CourseNotFound from './components/CourseNotFound';
+
+const prisma = new PrismaClient();
+
+type Course = {
+	id: number,
+	name: string
+}
+
+export async function getServerSideProps() {
+    const courses = await prisma.Courses.findMany();
+
+    return {
+		props: {
+			courses: courses
+		}
+	};
+};
+
+const Home: NextPage = (courses: any) => {
+	const router = useRouter();
+	const courseEntered = (router.query).course!.toString();
+
+	// Checks whether the course exists
+	let isACourse = false;
+	courses.courses.forEach((course: Course) => {
+		if(course.name === courseEntered) {
+			isACourse = true;			
+		}
+	});
+	
+	return (
+		<div>
+			<Logo />
+			<div className="flex items-center justify-center">
+				{
+					isACourse ? <Form course={courseEntered} /> : <CourseNotFound />
+				}
+			</div>
+		</div>
+	)
+}
+
+export default Home
