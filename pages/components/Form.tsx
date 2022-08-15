e12c4ecf1ccc230b5606d89d9d20ec0a000bc6ae
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-15 19:11:39 +0100

    Added a course prop

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 import Title from './Title'
 
-export default function Form() {
+type Props = {
+    course: string
+}
+
+export default function Form(props: Props) {
 
     const inputStyle = "rounded-md bg-green font-bold placeholder-neutral-200 text-white text-2xl w-full p-2 pl-2";
 
     return (
         <div className="flex flex-col justify-between items-center h-80 mt-4">
-            <Title />
+            <Title title={props.course} />
             <input type="text" placeholder="Your Name" className={inputStyle} />
             <input type="text" placeholder="Your Company" className={inputStyle} />
             <button className="bg-darkblue text-2xl w-fit p-1 px-2 font-bold text-white rounded-md">Submit</button>
