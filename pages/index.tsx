1d0d0e42155e9d520a4c43a9e279537445289ae2
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-15 21:07:53 +0100

    Added instructions on how users can enter their data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
 import type { NextPage } from 'next'
 
 import Logo from './components/Logo'
-import Form from './components/Form'
+import Title from './components/Title'
 
 const Home: NextPage = () => {
+
+	const paragraphStyling = "text-green text-lg w-8/12 font-bold";
+
 	return (
 		<div>
 			<Logo />
-			<div className="flex items-center justify-center">
-				<Form />
+			<div className="flex flex-col items-center justify-center">
+				<Title title="No Course Selected" showDate={false} />
+				<p className={paragraphStyling}>
+					In order to submit your information to get your certificate,
+					you must go to the Evolve Training office and request the QR
+					code for your course.
+				</p>
+				<br />
+				<p className={paragraphStyling}>
+					Please scan that QR code - or go to the URL below it - and fill
+					out your information in order to get your certificate.
+				</p>
 			</div>
 		</div>
 	)
 }
 
-export default Home
+export default Home;
