619c00e3a0842926730f37137709ecc3bc004c7b
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-18 23:21:29 +0100

    Added Course.tsx

---
+++ pages/components/Course.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Course() {
+    return (
+        <div className="flex justify-center hover:cursor-pointer items-center bg-green w-56 h-56 rounded-lg mr-6 mb-6">
+            <h1 className="font-bold select-none text-center text-3xl w-full text-white">Accident Investigation</h1>
+        </div>
+    )
+}
