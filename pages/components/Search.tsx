62ea4861f8b27e4b4c008caea02897edb331eb3f
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-18 23:26:10 +0100

    Changed margin and increased font size

--- pages/components/Search.tsx
+++ pages/components/Search.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Search() {
     return (
         <div>
-            <input type="text" className="bg-darkblue pl-1 rounded-md text-lg font-bold text-white place-grey-100 mb-2" placeholder="Search..." />
+            <input type="text" className="bg-darkblue pl-1 rounded-md text-xl font-bold text-white place-grey-100 mb-5" placeholder="Search..." />
         </div>
     )
 }
