cf96a114f9d4b98f62de00004052e29fcbf6c822
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-18 23:37:16 +0100

    Changed the position of search and trainees button

--- pages/components/CourseSelect.tsx
+++ pages/components/CourseSelect.tsx
@@ -5,11 +5,11 @@ import Search from './Search'
 
 export default function CourseSelect() {  
     return (
-        <div className="flex justify-center items-center">
-            <div className="flex flex-col justify-center items-center">
-                <div className="lg:flex">
+        <div className="flex justify-center items-center mt-10">
+            <div className="flex flex-col items-center">
+                <div className="lg:flex justify-between w-6.9/10">
                     <Search />
-                    <button className="bg-darkblue px-1 rounded-md text-white font-bold mb-2">All Trainees</button>
+                    <button className="bg-darkblue px-1 rounded-md text-white font-bold mb-2 mr-6">All Trainees</button>
                 </div>
                 <div className="flex flex-wrap justify-center items-center w-9/12">
                     <Course />
