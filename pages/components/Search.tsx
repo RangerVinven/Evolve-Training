a88d45f4a198e1289640fb5c56a9d73f7e4a29a0
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-18 23:22:11 +0100

    Added Search.tsx

---
+++ pages/components/Search.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Search() {
+    return (
+        <div>
+            <input type="text" className="bg-darkblue pl-1 rounded-md text-lg font-bold text-white place-grey-100 mb-2" placeholder="Search..." />
+        </div>
+    )
+}
