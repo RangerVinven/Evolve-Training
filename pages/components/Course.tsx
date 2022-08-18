ef180c54704253da53084407f393ecda08a8e8cc
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-18 23:57:48 +0100

    Added props

--- pages/components/Course.tsx
+++ pages/components/Course.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Course() {
+type Props = {
+    course: string
+}
+
+export default function Course(props: Props) {
     return (
         <div className="flex justify-center hover:cursor-pointer items-center bg-green w-56 h-56 rounded-lg mr-6 mb-6">
-            <h1 className="font-bold select-none text-center text-3xl w-full text-white">Accident Investigation</h1>
+            <h1 className="font-bold select-none text-center text-3xl w-full text-white">{props.course}</h1>
         </div>
     )
 }
