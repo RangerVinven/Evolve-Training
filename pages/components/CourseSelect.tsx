b5d757fcf6a57694e0e0d287cfb28f2b96750ba9
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 20:06:14 +0100

    Added /api/GetCourses call and a loading screen

--- pages/components/CourseSelect.tsx
+++ pages/components/CourseSelect.tsx
@@ -4,31 +4,60 @@ import BackButton from './BackButton'
 import Course from './Course'
 import Search from './Search'
 
+import { prisma } from "../../lib/prisma";
+
 type Course ={
     id: number
     name: string
 }
 
-export default function CourseSelect(props: any) {
+// export async function getServerSideProps() {
+// 	const courses = await prisma.courses.findMany({orderBy: [{ name: "asc" }]});
     
-    return (
-        <div className="flex justify-center items-center mt-10">
-            <div className="flex flex-col items-center">
-                <div className="lg:flex md:flex justify-between items-end w-6.9/10 ">
-                    <div className="flex flex-col items-start">
-                        <BackButton previousPage="/" isInTitle={false} />
-                        <Search />
+// 	return {
+// 		props: {
+// 			courses
+// 		}
+// 	}
+// }
+
+export default function CourseSelect(props: any) {    
+
+    let [courses, setCourses] = React.useState<Course[]>([]);
+    
+    React.useEffect(() => {
+        fetch("/api/GetCourses").then(res => res.json()).then(data => {
+            setCourses(data.courses);
+            console.log(courses);
+        });
+    }, []);
+    
+    if(courses.length === 0) {
+        return (
+            <div className="flex justify-center items-center">
+                <div className="text-3xl">Loading...</div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="flex justify-center items-center mt-10">    
+                <div className="flex flex-col items-center">
+                    <div className="lg:flex md:flex justify-between items-end w-6.9/10">
+                        <div className="flex flex-col items-start">
+                            <BackButton previousPage="/" isInTitle={false} />
+                            <Search />
+                        </div>
+                        <button className="h-10 bg-darkblue px-1 rounded-md text-white font-bold mb-2 mr-6">All Trainees</button>
                     </div>
-                    <button className="h-10 bg-darkblue px-1 rounded-md text-white font-bold mb-2 mr-6">All Trainees</button>
-                </div>
-                <div className="flex flex-wrap justify-center items-center w-9/12">
-                    {
-                        props.courses.map((course: Course) => {
-                            return <Course key={course.id} course={course.name} option={props.option} />
-                        })
-                    }
+                    <div className="flex flex-wrap justify-center items-center w-9/12 ">
+                        {
+                            courses.map((course: Course) => {
+                                return <Course key={course.id} course={course.name} option={props.option} />
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
