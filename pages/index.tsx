bee478d56b77cc1eec5d8e97d35116549ee58ba6
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 01:33:32 +0100

    Added router redirects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
 import type { NextPage } from 'next'
+import { NextRouter, useRouter } from 'next/router'
 
 import Logo from './components/Logo'
 import Title from './components/Title'
 
-const Home: NextPage = () => {
+const Home: NextPage = () => {	
 
+	const router = useRouter();
 	const buttonStyling = "bg-green text-white font-bold text-4xl px-7 py-3 mr-7 mb-7 rounded-md";
+
+	const redirectUser = (router: NextRouter, option: string) => {
+		router.push(`/courses/${option}`);
+	}
 
 	return (
 		<div>
 			<Logo />
 			<div className="flex flex-col items-center justify-center h-450">
 				<div className="flex flex-wrap md:flex-none lg:flex-none items-center justify-center">
-					<button className={buttonStyling}>Sign In</button>
-					<button className={buttonStyling}>Sign Out</button>
+					<button className={buttonStyling} onClick={() => redirectUser(router, "signin")}>Sign In</button>
+					<button className={buttonStyling} onClick={() => redirectUser(router, "signout")}>Sign Out</button>
 				</div>
-				<button className={buttonStyling}>Registered</button>
+				<button className={buttonStyling} onClick={() => redirectUser(router, "registered")}>Registered</button>
 			</div>
 		</div>
 	)
