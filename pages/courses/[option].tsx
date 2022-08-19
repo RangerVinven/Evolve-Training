887efeed1edd248fe71496c3ac8cf79c83d255e5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 01:09:38 +0100

    Added the options query

---
+++ pages/courses/[option].tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import Logo from '../components/Logo';
+import CourseSelect from '../components/CourseSelect';
+
+import { prisma } from "../../lib/prisma";
+import Title from '../components/Title';
+
+export async function getServerSideProps() {
+	const courses = await prisma.courses.findMany({orderBy: [{ name: "asc" }]});
+
+	return {
+		props: {
+			courses
+		}
+	}
+}
+
+export default function Courses(props: any) {
+
+	const router = useRouter();
+    const option = router.query.option!.toString();
+
+	
+	if(option === "signin" || option === "signup" || option === "registered") {
+		return (
+			<div>
+				<Logo />
+				<div className="flex justify-center items-center">
+					<CourseSelect courses={props.courses} />
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<Logo />
+				<div className="flex justify-center items-center">
+					<Title title="Error: Only 'signin', 'signup' and 'registered' is allowed" showDate={false} />
+				</div>
+			</div>
+		)
+	}
+}
