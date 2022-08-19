04bef608ea34392f77d6d2eb8cbd04ab8a2d0912
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 01:20:58 +0100

    Added a custom height

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       colors: {
         darkblue: "#1F5C78",
         green: "#1DA39A"
+      },
+      width: {
+        "6.9/10": "69%",
+      },
+      height: {
+        "450": "450px",
       }
     },
   },
