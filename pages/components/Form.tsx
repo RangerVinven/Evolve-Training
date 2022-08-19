44bc7308e2fbfcefd58ca052f346b24b4ec31eaa
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 17:50:33 +0100

    Added content-type and changed body variables to api call

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -9,26 +9,26 @@ type Props = {
 export default function Form(props: Props) {
 
     let [name, setName] = React.useState("")
-    let [course, setCourse] = React.useState("")
+    let [company, setCompany] = React.useState("")
     const inputStyle = "rounded-md bg-green font-bold placeholder-neutral-200 text-white text-2xl w-full p-2 pl-2";
 
     return (
         <div className="flex flex-col justify-between items-center h-80 mt-4">
             <Title title={props.course} showDate={true} />
             
-            <input type="text" placeholder="Your Name" className={inputStyle} onChange={(e) => {
-                setName(e.target.value);
-            }} />
-            <input type="text" placeholder="Your Company" className={inputStyle} onChange={(e) => {
-                setCourse(e.target.value);
-            }} />
-            <button onClick={() => {
+            <input type="text" placeholder="Your Name" className={inputStyle} onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="Your Company" className={inputStyle} onChange={(e) => setCompany(e.target.value)} />
+            <button onClick={() => { 
                 fetch("http://localhost:3000/api/AddClient", {
                     method: "POST",
                     body: JSON.stringify({
-                        name: name,
-                        course: course
-                    })
+                        "name": name,
+                        "company": company,
+                        "course": props.course
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                   }
                 });
             }} className="bg-darkblue text-3xl w-fit p-1 px-3 font-bold text-white rounded-md">Sign In</button>
         </div>
