68693ec8cdce7afbe6b11bf578e74577b32592ac
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 18:13:40 +0100

    Added a showBackButton and previousPage prop

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 type Props = {
     title: string,
-    showDate: boolean
+    showDate: boolean,
+
+    // For the back button
+    showBackButton?: boolean,
+    previousPage?: string
 }
 
 export default function Title(props: Props) {
 
     const date = new Date();
+    const router = useRouter();
 
     // Returns the month number with "0" in front if it is less than 10
     const getMonth = (month: number) => {
@@ -18,9 +24,18 @@ export default function Title(props: Props) {
         }
     }
 
-    return (
-        <div>
-            <h1 className="text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
-        </div>
-    );
+    if(props.showBackButton) {
+        return (
+            <div className="flex justify-center items-center">
+                <button className="bg-darkblue text-3xl w-fit p-1 px-3 font-bold text-white rounded-md mr-2" onClick={() => {
+                    if(props.previousPage !== undefined) {
+                        router.push(props.previousPage);
+                    }
+                }}>Back</button>
+                <h1 className="text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
+            </div>
+        )
+    } else {
+        return <h1 className="text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
+    }
 }
