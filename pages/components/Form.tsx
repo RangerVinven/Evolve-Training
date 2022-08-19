e320667d24f3991567cb80fe49f5647dabc67fd4
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-19 02:15:14 +0100

    Added state variables and signIn function

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -8,15 +8,29 @@ type Props = {
 
 export default function Form(props: Props) {
 
+    let [name, setName] = React.useState("")
+    let [course, setCourse] = React.useState("")
     const inputStyle = "rounded-md bg-green font-bold placeholder-neutral-200 text-white text-2xl w-full p-2 pl-2";
 
     return (
         <div className="flex flex-col justify-between items-center h-80 mt-4">
             <Title title={props.course} showDate={true} />
             
-            <input type="text" placeholder="Your Name" className={inputStyle} />
-            <input type="text" placeholder="Your Company" className={inputStyle} />
-            <button className="bg-darkblue text-2xl w-fit p-1 px-2 font-bold text-white rounded-md">Sign In</button>
+            <input type="text" placeholder="Your Name" className={inputStyle} onChange={(e) => {
+                setName(e.target.value);
+            }} />
+            <input type="text" placeholder="Your Company" className={inputStyle} onChange={(e) => {
+                setCourse(e.target.value);
+            }} />
+            <button onClick={() => {
+                fetch("http://localhost:3000/api/AddClient", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name: name,
+                        course: course
+                    })
+                });
+            }} className="bg-darkblue text-3xl w-fit p-1 px-3 font-bold text-white rounded-md">Sign In</button>
         </div>
     )
 }
