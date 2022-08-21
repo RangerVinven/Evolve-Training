1d0e85139832121451a4bbb1a5954c93a270b342
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-21 22:29:41 +0100

    Moved form into SignOutForm.tsx

--- pages/components/SignOutForm.tsx
+++ pages/components/SignOutForm.tsx
@@ -1,40 +1,67 @@
-import React from 'react'
-// import Logo from './Logo';
-// import Title from './Title';
+import React, { useEffect } from 'react'
+import ReactLoading from 'react-loading';
+import Logo from './Logo';
 
-// type Props ={
-//     course: string
-//     clients: {}[]
-// }
+type Props ={
+    course: string,
+}
 
-// export default function SignOutForm(props: Props) {
-//     return (
-//         <div>
-//             <Logo />
-//             <div className="flex justify-center items-center h-96">
-//                 <div className="flex flex-col items-center justify-center 6/12">
-//                     <div className="mb-12">
-//                             <Title title={props.course} showDate={true} showBackButton={true} previousPage="/" />
-//                     </div>
-//                     <select defaultValue="Your Name" className="w-96 h-12 bg-green pl-1 rounded-md text-white font-bold text-2xl" name="client">
-//                         <option disabled>Your Name</option>
-//                         {
-//                             props.clients.map((client: any) => {
-//                                 return (
-//                                     <option key={client.id} value={client.name}>{client.name} - {client.company}</option>
-//                                 )
-//                             })
-//                         }
-//                     </select>
-//                     <button className="bg-darkblue text-2xl w-fit p-1 px-2 mt-8 font-bold text-white rounded-md">Sign Out</button>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+export default function SignOutForm(props: Props) {
 
-export default function SignOutForm() {
-  return (
-    <div>SignOutForm</div>
-  )
+	let [clients, setClients] = React.useState([{
+		"Clients Not Loaded": "Clients Not Loaded",
+	}]);
+
+	useEffect(() => {
+		fetch("/api/GetClientsOfACourse", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				"course": props.course
+			})
+		}).then(res => res.json()).then(data => {
+			setClients(data.clients);                                        
+		});
+	}, [])
+
+	if(clients.length === 1) {
+		if(clients[0].hasOwnProperty("Clients Not Loaded")) {
+			return (
+				<div>
+                    <Logo />
+                    <div className="h-450 flex justify-center items-center">
+                        <ReactLoading type="spinningBubbles" color="#1F5C78" height={100} width={100} />
+                    </div>
+                </div>
+			);
+		} else {
+			return (
+				<select defaultValue="Your Name" className="w-96 h-12 bg-green pl-1 rounded-md text-white font-bold text-2xl" name="client">
+					<option disabled>Your Name</option>
+					{
+						clients.map((client: any) => {
+							return (
+								<option key={client.id} value={client.name}>{client.name} - {client.company}</option>
+							)
+						})
+					}
+				</select>
+			);
+		}
+	} else {
+		return (
+			<select defaultValue="Your Name" className="w-96 h-12 bg-green pl-1 rounded-md text-white font-bold text-2xl" name="client">
+				<option disabled>Your Name</option>
+				{
+					clients.map((client: any) => {
+						return (
+							<option key={client.id} value={client.name}>{client.name} - {client.company}</option>
+						)
+					})
+				}
+			</select>
+		)
+	}
 }
