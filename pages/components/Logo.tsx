5f30fc795157e26882399c22ff4d99b766743a75
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-21 21:34:37 +0100

    Changed the top margin

--- pages/components/Logo.tsx
+++ pages/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
     return (
         <div className="w-full flex items-center justify-center md:block lg:block">
-            <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-1" />
+            <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-2" />
         </div>
     )
 }
