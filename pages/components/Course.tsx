aceba08536241f077610dffcb708513726e30751
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-23 23:38:28 +0100

    Made the course boxes larger

--- pages/components/Course.tsx
+++ pages/components/Course.tsx
@@ -16,7 +16,7 @@ export default function Course(props: Props) {
                 course: props.course
             }
         }} >
-            <div className="flex justify-center hover:cursor-pointer items-center bg-green w-56 h-56 rounded-lg mr-6 mb-6">
+            <div className="flex justify-center hover:cursor-pointer items-center bg-green w-60 h-60 rounded-lg mr-6 mb-6">
                 <h1 className="font-bold select-none text-center text-3xl w-full text-white">{props.course}</h1>
             </div>
         </Link>
