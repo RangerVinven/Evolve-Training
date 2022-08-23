3ca4aaf08e2a18e817075468d7ab80e90d602d03
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-23 22:50:04 +0100

    Added toasts

--- pages/components/SignOutForm.tsx
+++ pages/components/SignOutForm.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react'
 import ReactLoading from 'react-loading';
-import Logo from './Logo';
 
 type Props ={
     course: string,
+	toast: any
 }
 
 type Clients = {
 	clients: Array<{}>,
 	setSelectedClientID: Function,
-	selectedClientID: number
+	selectedClientID: number,
+	toast: any
 }
 
 export default function SignOutForm(props: Props) {
@@ -38,7 +39,6 @@ export default function SignOutForm(props: Props) {
 		if(clients[0].hasOwnProperty("Clients Not Loaded")) {
 			return (
 				<div>
-                    <Logo />
                     <div className="h-450 flex justify-center items-center">
                         <ReactLoading type="spinningBubbles" color="#1F5C78" height={100} width={100} />
                     </div>
@@ -46,22 +46,22 @@ export default function SignOutForm(props: Props) {
 			);
 		} else {
 			return (
-				<SelectAndSubmitComponent clients={clients} setSelectedClientID={setSelectedClientID} selectedClientID={selectedClientID}  />
+				<SelectAndSubmitComponent toast={props.toast} clients={clients} setSelectedClientID={setSelectedClientID} selectedClientID={selectedClientID}  />
 			);
 		}
 	} else {
 		return (
-			<SelectAndSubmitComponent clients={clients} setSelectedClientID={setSelectedClientID} selectedClientID={selectedClientID} />
+			<SelectAndSubmitComponent toast={props.toast} clients={clients} setSelectedClientID={setSelectedClientID} selectedClientID={selectedClientID} />
 		);
 	}
-}
+};
 
 function SelectAndSubmitComponent(props: Clients) {
 	return (
-		<div>
+		<div className="flex flex-col justify-center items-center">
 			<select onChange={(event) => {
 				props.setSelectedClientID(Number(event.target.value));
-			}} defaultValue="Your Name" className="w-96 h-12 bg-green pl-1 rounded-md text-white font-bold text-2xl" name="client">
+			}} defaultValue="Your Name" className="w-96 h-12 mb-5 bg-green pl-1 rounded-md text-white font-bold text-2xl" name="client">
 				<option disabled>Your Name</option>
 				{
 					props.clients.map((client: any) => {
@@ -80,8 +80,25 @@ function SelectAndSubmitComponent(props: Clients) {
 					body: JSON.stringify({
 						"clientID": props.selectedClientID,
 					})
+				}).then(res => {
+					if(res.status === 200) {
+						props.toast("👍 You're Signed Out", {
+                            clickClosable: true,
+                            render: (text: string) => <div className="bg-lime-500 text-2xl text-white font-bold px-10 py-2 rounded-lg">{text}</div>
+                        });
+					} else if (res.status === 400) {
+						props.toast("👎 Client ID Not Found", {
+							clickClosable: true,
+							render: (text: string) => <div className="bg-red-500 text-2xl text-white font-bold px-10 py-2 rounded-lg">{text}</div>
+						});
+					} else {
+						props.toast("👎 Something Went Wrong", {
+							clickClosable: true,
+							render: (text: string) => <div className="bg-red-500 text-2xl text-white font-bold px-10 py-2 rounded-lg">{text}</div>
+						});
+					}
 				});
 			}} className="bg-darkblue text-2xl w-fit p-1 px-2 mt-8 font-bold text-white rounded-md">Sign Out</button>
 		</div>
 	)
-}
+};
