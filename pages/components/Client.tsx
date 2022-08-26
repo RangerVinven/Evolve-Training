e06d3f6ab2a7051c38c6d16e53fbb30e9af6f244
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-26 23:53:08 +0100

    Added styling and template text

--- pages/components/Client.tsx
+++ pages/components/Client.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Client() {
   return (
-    <div>Client</div>
+    <div className="bg-green rounded-md flex flex-col p-2 font-bold text-white">
+        <h1>Name: John Doe</h1>
+        <h1>Company: Evolve Training</h1>
+        <h1>Course: Forklift Handling</h1>
+    </div>
   )
 }
