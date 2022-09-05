72cc570f1cebd76339e9423b0387de785fa2a8f3
RangerVinven <danielmcpherson1195@gmail.com>
2022-09-05 15:25:42 +0100

    Added a /api/SearchCourse call

--- pages/components/Search.tsx
+++ pages/components/Search.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export default function Search() {
+type Props = {
+    setCourses: Function
+}
+
+export default function Search(props: Props) {
+
+    const searchCourses = (search: string) => {
+        fetch("/api/SearchCourse", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                course: search
+            })
+        }).then(res => res.json()).then(data => {
+            props.setCourses(data.courses);            
+        })
+    }
+
     return (
         <div>
-            <input type="text" className="bg-darkblue pl-1 rounded-md text-xl font-bold text-white place-grey-100 mb-2" placeholder="Search..." />
+            <input onChange={(e) => {
+                searchCourses(e.target.value);                
+            }} type="text" className="bg-darkblue pl-1 rounded-md text-xl font-bold text-white place-grey-100 mb-2" placeholder="Search..." />
         </div>
     )
 }
