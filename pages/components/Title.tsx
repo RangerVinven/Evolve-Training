4a9959472c0327e411fdf7786acbf6179a05397a
RangerVinven <danielmcpherson1195@gmail.com>
2022-09-05 15:56:10 +0100

    Added absolute positioning

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -24,9 +24,11 @@ export default function Title(props: Props) {
     }
 
     return (
-        <div className="flex justify-center items-center">
-            <BackButton showBackButton={props.showBackButton} isInTitle={true} previousPage={props.previousPage === undefined ? "/" : props.previousPage} />
-            <h1 className="text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
+        <div className="absolute left-1/2 top-11 -translate-x-1/2">
+            <div className="flex justify-center items-center">
+                <BackButton showBackButton={props.showBackButton} isInTitle={true} previousPage={props.previousPage === undefined ? "/" : props.previousPage} />
+                <h1 className="text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
+            </div>
         </div>
     )
 }
