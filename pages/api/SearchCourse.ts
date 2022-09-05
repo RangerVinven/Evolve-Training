c15d1d087cb73d8d36d789f24c4de76b700d300e
RangerVinven <danielmcpherson1195@gmail.com>
2022-09-05 13:59:42 +0100

    Added SearchCourse.ts

---
+++ pages/api/SearchCourse.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from "../../lib/prisma";
+
+type Data = {
+  courses?: {}[],
+  error?: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const course = req.body.course;
+    
+    if(req.method !== "POST") return res.status(405).json({ error: "Only GET is allowed" });
+
+    prisma.courses.findMany({
+        where: {
+            name: {
+                startsWith: course
+            }
+        }
+    }).then((courses: any) => {
+        return res.status(200).json({
+            courses: courses
+        });
+    }).catch(err => {
+        return res.status(500).json({
+            error: "Something Went Wrong"
+        })
+    });
+}
