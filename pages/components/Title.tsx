e24eb481cf152cc7aa48bdd8de25d8f2ba081ac2
RangerVinven <danielmcpherson1195@gmail.com>
2022-09-06 20:58:45 +0100

    Added a 0 to the start of the day if it's < 0

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -15,6 +15,15 @@ export default function Title(props: Props) {
     const date = new Date();
 
     // Returns the month number with "0" in front if it is less than 10
+    const getDay = (day: number) => {
+        if(day < 10) {
+            return "0" + day.toString();
+        } else {
+            return day.toString();
+        }
+    }
+
+    // Returns the month number with "0" in front if it is less than 10
     const getMonth = (month: number) => {
         if(month < 10) {
             return "0" + month.toString();
@@ -27,7 +36,7 @@ export default function Title(props: Props) {
         <div className="w-full absolute left-1/2 top-28 lg:top-11 -translate-x-1/2">
             <div className="flex justify-center items-center">
                 <BackButton showBackButton={props.showBackButton} isInTitle={true} previousPage={props.previousPage === undefined ? "/" : props.previousPage} />
-                <h1 className="text-2xl md:text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + date.getDate() + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
+                <h1 className="text-2xl md:text-4xl text-darkblue font-bold">{props.title + (props.showDate ? " - " + getDay(date.getDate()) + "/" + (getMonth(date.getMonth() + 1)) + "/" + date.getFullYear() : "")}</h1>
             </div>
         </div>
     )
