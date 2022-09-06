a7e5f3cdf27c23e546da183c0006c6c2b35189b9
RangerVinven <danielmcpherson1195@gmail.com>
2022-09-06 22:06:24 +0100

    Changed to useEffect from getServerSide props

--- pages/allTrainees.tsx
+++ pages/allTrainees.tsx
@@ -6,58 +6,8 @@ import Client from './components/Client';
 
 import { prisma } from "../lib/prisma";
 
-function getTraineesFromCourses(courses: any[], clients: any[]) {
-    let clientsOfCourse = [];
-    let newCoursesAndClients = [];
-
-    // Loops through the courses
-    for(let i = 0; i < courses.length; i++) {
-        let courseID = courses[i].id;
-
-        // Adds the clients for the current course
-        for (let x = 0; x < clients.length; x++) {
-            if(clients[x].course === courseID) {
-                clientsOfCourse.push(clients[x]);
-            } else if (clients[x].course === courseID) {
-                break;
-            }
-        }     
-        
-        if(clientsOfCourse.length !== 0) newCoursesAndClients.push({
-            course: courses[i].name,
-            clients: clientsOfCourse
-        });
-
-        clientsOfCourse = [];
-    }  
-    
-    return newCoursesAndClients;    
-}
-
-export async function getServerSideProps(context: any) {
-    let courses: Array<{}> = []
-    let clients: Array<{}> = []
-
-    await prisma.courses.findMany().then(res => courses = res);
-    await prisma.clients.findMany({
-        orderBy: [
-            {
-                course: "asc",
-            },
-            {
-                name: "asc"
-            }
-        ]
-    }).then(res => clients = res);    
-
-    const coursesAndClients = getTraineesFromCourses(courses, clients);
-
-    return {
-        props: {
-            coursesAndClients: coursesAndClients
-        }
-    };
-}
+import ReactLoading from 'react-loading';
+import toast from 'react-simple-toasts';
 
 type Props = {
     coursesAndClients: Array<{
@@ -74,39 +24,69 @@ type Course = {
 export default function AllTrainees(props: Props) {
 
     let [isMobile, setIsMobile] = React.useState(false);
+    let [coursesAndClients, setCoursesAndClients] = React.useState([]);
+    let [loading, setLoading] = React.useState(true);
 
     React.useEffect(() => {
         const userAgent = typeof window.navigator === "undefined" ? "" : navigator.userAgent;
         const mobile = Boolean(userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i));
         setIsMobile(mobile);
+
+        fetch("/api/AllTrainees").then((res: any) => {
+            setCoursesAndClients(res.body.coursesAndClients);
+            setLoading(false);
+        });
+
+
+        fetch("/api/GetClientsOfACourse").then(res => res.json()).then(res => {
+            setCoursesAndClients(res);
+            setLoading(false);
+        }).catch(err => {
+            toast("👎 Something Went Wrong", {
+                clickClosable: true,
+                render: (text: string) => <div className="bg-red-500 text-2xl text-white font-bold px-10 py-2 rounded-lg">{text}</div>
+            });
+        });
+
     }, [])
     
 
-    return (
-        <div>
-            <Logo />
-            <div className="flex justify-center items-center mt-10">
-                <div className="flex flex-col items-center justify-center w-9/12">
-                    <div className="mb-12">
-                        <Title title="All Trainees" showDate={!isMobile} showBackButton={true} previousPage="/" />
-                    </div>
-
-                    {
-                        props.coursesAndClients.map((course: any) => {        
-                            return <div key={course.name} className="flex flex-col justify-center items-center">
-                                <h3 className="text-darkblue text-center text-3xl font-semibold mb-1">{course.course}</h3>
-                                <div className="flex flex-wrap justify-center mb-5">
-                                    {
-                                        course.clients.map((client: any)=> {
-                                            return <Client key={client.name+"-"+client.company} name={client.name} company={client.company} />
-                                        })
-                                    }
-                                </div>
-                            </div>
-                        })
-                    }
+    if(loading) {
+        return (
+            <div>
+                <Logo />
+                <div className="absolute left-2/4 top-2/4 transform -translate-x-1/2 -translate-y-1/2">
+                    <ReactLoading type="spinningBubbles" color="#1F5C78" height={125} width={125} />
                 </div>
             </div>
-        </div>
-    )
+        );
+    } else {
+        return (
+            <div>
+                <Logo />
+                <div className="flex justify-center items-center mt-10">
+                    <div className="flex flex-col items-center justify-center w-9/12">
+                        <div className="mb-12">
+                            <Title title="All Trainees" showDate={!isMobile} showBackButton={true} previousPage="/" />
+                        </div>
+    
+                        {
+                            coursesAndClients.map((course: any) => {        
+                                return <div key={course.name} className="flex flex-col justify-center items-center">
+                                    <h3 className="text-darkblue text-center text-3xl font-semibold mb-1">{course.course}</h3>
+                                    <div className="flex flex-wrap justify-center mb-5">
+                                        {
+                                            course.clients.map((client: any)=> {
+                                                return <Client key={client.name+"-"+client.company} name={client.name} company={client.company} />
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
