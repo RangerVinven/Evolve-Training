862d5a28b5970727383d10194a0e3db9668fd29c
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-09-12 14:45:07 +0100

    Removed duplicate items-center

--- pages/components/Course.tsx
+++ pages/components/Course.tsx
@@ -15,7 +15,7 @@ export default function Course(props: Props) {
                 course: props.course
             }
         }} >
-            <div className="flex justify-center items-center hover:cursor-pointer items-center bg-green w-60 h-60 p-10 rounded-lg mr-6 mb-6">
+            <div className="flex justify-center hover:cursor-pointer items-center bg-green w-60 h-60 rounded-lg mr-6 mb-6">
                 <h1 className="font-bold select-none text-center text-3xl w-full text-white">{props.course}</h1>
             </div>
         </Link>
