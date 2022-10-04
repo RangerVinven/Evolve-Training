96535440e2a1ffbe975c4c081f12f2a3c36f6000
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-04 20:48:49 +0100

    Added link to / on press

--- pages/components/Logo.tsx
+++ pages/components/Logo.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Logo() {
     return (
-        <div className="w-full flex items-center justify-center md:block lg:block">
-            <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-2" />
-        </div>
+        <Link href={{
+            pathname: '/',
+        }}>
+            <div className="w-full flex items-center justify-center md:block lg:block hover:cursor-pointer">
+                <img src="/logo.png" alt="Evolve Training Logo" className="h-20 ml-2 mt-2" />
+            </div>
+        </Link>
     )
 }
