7e31950ee11e1509ddaaaf4de54f340e5407aed5
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-04 20:54:58 +0100

    Added blank signin page

---
+++ pages/staff/signin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Logo from '../components/Logo'
+
+export default function signin() {
+    return (
+        <div>
+            <div className="absolute">
+				<Logo />
+			</div>
+        </div>
+    )
+}
