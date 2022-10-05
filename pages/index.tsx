24bc5749c5c29713671dafc411c00520363ecb36
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-05 16:03:21 +0100

    Added Staff button

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,15 @@ const Home: NextPage = () => {
 	return (
 		<div>
 			<Logo />
+
+			<div className="absolute top-3/4">
+				<Link href={{
+					pathname: "/staff"
+				}}>
+					<button className="flex justify-center items-center bg-darkblue text-2xl w-fit md:p-1 md:px-3 px-1 pb-1 font-bold text-white rounded-md mr-2">Staff</button>
+				</Link>
+			</div>
+
 			<div className="m-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 				<div className="flex flex-col">
 					<div className="flex flex-wrap flex-none lg:flex-none items-center justify-center">
