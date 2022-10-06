0261e1819b7ff695f05f1f26af2a2e1945184b2a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-06 14:35:38 +0100

    Added signed in staff to DOM

--- pages/staff/signedin.tsx
+++ pages/staff/signedin.tsx
@@ -3,22 +3,28 @@ import React, { useEffect, useState } from 'react'
 import Logo from '../components/Logo'
 import Title from '../components/Title'
 
+interface Staff {
+    id: number
+    Name: string
+    SignedIn: Boolean
+}
+
 export default function signedin() {
 
-    let [signedInStaffState, setSignedInStaffState] = useState<{}>([]);
+    let [signedInStaffState, setSignedInStaffState] = useState<Array<{}>>([]);
 
     useEffect(() => {
         // Gets all the staff
         fetch("/api/staff/SeeStaff").then(res => res.json()).then(res => {
             // Loops and adds the signed in ones to the signedInStaff state
-            let signedInStaff = []
+            let signedInStaff: Array<{}> = [];
             for(let staff of res.staff) {
                 if(staff.SignedIn) signedInStaff.push(staff);
             }
 
             setSignedInStaffState(signedInStaff);
         });
-    });
+    }, []);
 
     return (
         <div>
@@ -26,6 +32,15 @@ export default function signedin() {
                 <Logo />
             </div>
             <Title title="Signed In Staff" showDate={true} showBackButton={true} previousPage="/staff" />
+
+            <div className="absolute left-1/2 -translate-x-1/2 mt-32">
+                {
+                    signedInStaffState.map((staff: any) => <p className="bg-green text-center rounded-md flex flex-col p-2 mr-4 mb-4 font-bold text-white" 
+                    key={staff.Name}>
+                        {staff.Name}
+                    </p>)
+                }
+            </div>
         </div>
     )
 }
