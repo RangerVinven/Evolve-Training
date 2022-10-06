1d44fc32c92425cf29637a5087980a2c5f18a501
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-06 13:47:44 +0100

    Added /api/staff/SeeStaff call

--- pages/staff/signedin.tsx
+++ pages/staff/signedin.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Logo from '../components/Logo'
 import Title from '../components/Title'
 
 export default function signedin() {
-  return (
-    <div>
-        <div className="absolute">
-            <Logo />
+
+    let [signedInStaffState, setSignedInStaffState] = useState<{}>([]);
+
+    useEffect(() => {
+        // Gets all the staff
+        fetch("/api/staff/SeeStaff").then(res => res.json()).then(res => {
+            // Loops and adds the signed in ones to the signedInStaff state
+            let signedInStaff = []
+            for(let staff of res.staff) {
+                if(staff.SignedIn) signedInStaff.push(staff);
+            }
+
+            setSignedInStaffState(signedInStaff);
+        });
+    });
+
+    return (
+        <div>
+            <div className="absolute">
+                <Logo />
+            </div>
+            <Title title="Signed In Staff" showDate={true} showBackButton={true} previousPage="/staff" />
         </div>
-        <Title title="All Staff" showDate={true} showBackButton={true} previousPage="/staff" />
-        
-    </div>
-  )
+    )
 }
