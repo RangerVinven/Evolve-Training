10232a8daa1f1f3fc60c097166de42062c3980ca
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-10-10 15:09:48 +0100

    Styled staff button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,12 @@ import Logo from './components/Logo'
 
 const Home: NextPage = () => {	
 
-	// const buttonStyling = "bg-green text-white font-bold text-4xl px-7 py-3 mr-7 mb-7 rounded-md";
+	// const buttonStyling = "bg-green text-white font-bold text-4xl px-7 py-3 b-7 rounded-md";
 	const buttonStyling = "bg-green text-white font-bold text-4xl px-14 py-6 mr-7 mb-7 rounded-md";
 
 	return (
 		<div>
 			<Logo />
-
-			<div className="absolute top-3/4">
-				<Link href={{
-					pathname: "/staff"
-				}}>
-					<button className="flex justify-center items-center bg-darkblue text-2xl w-fit md:p-1 md:px-3 px-1 pb-1 font-bold text-white rounded-md mr-2">Staff</button>
-				</Link>
-			</div>
 
 			<div className="m-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 				<div className="flex flex-col">
@@ -42,6 +34,7 @@ const Home: NextPage = () => {
 							<button className={buttonStyling}>Sign Out</button>
 						</Link>
 					</div>
+
 					<Link href={{
 							pathname: '/courses',
 							query: {
@@ -49,6 +42,12 @@ const Home: NextPage = () => {
 							}
 						}}>
 						<button className="bg-green text-white font-bold text-2xl px-2 py-2 mr-7 mt-10 rounded-md">Registered</button>
+					</Link>
+
+					<Link href={{
+							pathname: "/staff"
+						}}>
+						<button className="border-2 border-green text-darkblue text-2xl px-2 py-2 mr-7 mt-10 rounded-md">Staff</button>
 					</Link>
 				</div>
 			</div>
